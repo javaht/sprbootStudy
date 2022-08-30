794eb70552d773c1ffeb1d5a2c4b34321f547893
root <java_ht@163.com>
2022-08-30 22:45:14 +0800

    Initial commit

---
+++ src/main/java/com/zht/demo/SprbootStudyApplication.java
@@ -0,0 +1,13 @@
+package com.zht.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprbootStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprbootStudyApplication.class, args);
+    }
+
+}
